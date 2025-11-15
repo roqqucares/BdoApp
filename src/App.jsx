e161be93bf5096abd09498496b7c6bf940a45215
roqqucares <roqqucares@gmail.com>
2025-11-15 08:21:50 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Pin from "./pages/Pin";
+import EmailOtp from "./pages/EmailOtp";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/otp" element={<Pin />} />
+          <Route path="/email-otp" element={<EmailOtp />} />
         </Routes>
       </Router>
     </div>
